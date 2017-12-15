85d8bcabcb0bfbae4f9185f02deb424da00de7f4
Porkipic <mas.antoine@gmail.com>
2017-12-15 12:06:02 -0500

    Update ADC.c

--- Libraries/ADC/ADC.c
+++ Libraries/ADC/ADC.c
@@ -4,12 +4,12 @@
 void initADC(){
     ADCSRA |= (1<<ADEN) & (0<<ADATE) & (1<<ADIE) & (7<<ADPS0);
 }
-void readADCbits(uint8_t analogPinToRead, uint8_t voltageRef){
+void startADC(uint8_t analogPinToRead, uint8_t voltageRef){
     ADMUX |= (voltageRef<<REFS0) & (1<<ADLAR) & (analogPinToRead<<0);
     ADCSRA |= (1<<ADSC);
 }
 
-void getADCValue(uint13_t &ADCValue){
+uint8_t getADCValue(){
     return *ADCValue = (ADCH<<2) & (ADCL);
 }
 
