fa27a60a653b2a0bbca84d30a3fb36ae1626f228
Porkipic <mas.antoine@gmail.com>
2017-12-15 12:05:03 -0500

    Update ADC.h

--- Libraries/ADC/ADC.h
+++ Libraries/ADC/ADC.h
@@ -5,7 +5,15 @@
 #define AVCC 1;
 #define INTERNAL 3;
 
+#define ADC0 0;
+#define ADC1 1;
+#define ADC2 2;
+#define ADC3 3;
+#define ADC4 4;
+#define ADC4 5;
+
 void initADC();
-uint16_t readADCbits(uint8_t analogPinToRead, uint8_t voltageRef);
+void startADC(uint8_t analogPinToRead, uint8_t voltageRef);
+uint8_t getADCValue(unint16_t *ADCValue);
 
 #endif
