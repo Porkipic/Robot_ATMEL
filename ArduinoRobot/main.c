fc6563a1bd95681f315522903020d1144cdbef40
Porkipic <mas.antoine@gmail.com>
2017-12-14 20:46:38 -0500

    Initial commit

---
+++ ArduinoRobot/main.c
@@ -0,0 +1,17 @@
+/*
+ * ArduinoRobot.c
+ *
+ * Created: 14/12/2017 20:36:53
+ * Author : Antoine
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
