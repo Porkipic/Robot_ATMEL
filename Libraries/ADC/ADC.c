4c79e4d57b08e83f7114f8cd62a084768ddb2a1f
Porkipic <mas.antoine@gmail.com>
2017-12-15 12:11:08 -0500

    Update ADC.c

--- Libraries/ADC/ADC.c
+++ Libraries/ADC/ADC.c
@@ -1,16 +1,20 @@
 #include "ADC.h"
 #include <avr/interrupt.h>
 
+volatile uint8_t = 0;
+
 void initADC(){
     ADCSRA |= (1<<ADEN) & (0<<ADATE) & (1<<ADIE) & (7<<ADPS0);
 }
 void startADC(uint8_t analogPinToRead, uint8_t voltageRef){
-    ADMUX |= (voltageRef<<REFS0) & (1<<ADLAR) & (analogPinToRead<<0);
+    ADMUX |= (voltageRef<<REFS0) & (1<<ADLAR) & analogPinToRead;
     ADCSRA |= (1<<ADSC);
 }
 
-uint8_t getADCValue(){
-    return *ADCValue = (ADCH<<2) & (ADCL);
+uint16_t getADCValue(){
+    uint16_t result = 0;
+    result = (ADCH<<2) & (ADCL);
+    return result;
 }
 
 ISR(ADC_vect){
